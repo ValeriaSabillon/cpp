d640a9e1475161dedcd25049997712dee7ce2ad9
Valeria Sabillón <vsabillon@unah.hn>
2021-02-02 15:10:01 -0600

    agregando ejercicio funciones int con parametros

---
+++ Ejercicios/Ejercicio30-Funcion int con parametros/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int sumar(int a,int b){
+    return a+b;
+}
+int restar(int a,int b){
+    return a-b;
+}
+int main(int argc, char const *argv[])
+{
+system("cls");
+int numero1=0;
+int numero2=0;
+
+cout<<"Ingrese el valor de a:";
+cin>>numero1;
+cout<<"Ingrese el valor de b:";
+cin>>numero2;
+
+cout<<endl;
+cout<<"el resultado de la suma es:"<<sumar(numero1,numero2);  
+cout<<endl;
+cout<<"El resultado de la resta es:"<<restar(numero1,numero2);
+    return 0;
+}
