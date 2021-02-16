ca0538e4c03323f88bcb0d8e15e4bc0367d7a70e
Valeria Sabillón <vsabillon@unah.hn>
2021-02-16 16:57:15 -0600

    agregando seleccion de juego

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -6,10 +6,24 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    
+    int juego=0;
+    cout<<"**************"<<endl;
+    cout<<"MENU DE JUEGOS"<<endl;
+    cout<<"**************"<<endl;
+    cout<<"Ingrese un numero del menu para seleccionar un juego: ";
+    cin>>juego;
     system("cls");
-
-    // starShip();
-    // snake();
-
+    if(juego==1){
+     
+    starShip();
+    
+    }
+     else if(juego==2){
+    
+    snake();
+      
+    }
+  
     return 0;
 }
