768556d1435151c2390d8fd977d42ec612117a89
Valeria Sabillón <vsabillon@unah.hn>
2021-02-20 12:17:21 -0600

    agregando cambios al proyecto I

--- Proyecto I Unidad/Codigo/main.cpp
+++ Proyecto I Unidad/Codigo/main.cpp
@@ -7,23 +7,28 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     
-    int juego=0;
+    int opcion=0;
     cout<<"**************"<<endl;
     cout<<"MENU DE JUEGOS"<<endl;
     cout<<"**************"<<endl;
     cout<<"Ingrese un numero del menu para seleccionar un juego: ";
-    cin>>juego;
+    cin>>opcion;
     system("cls");
-    if(juego==1){
-     
-    starShip();
-    
-    }
-     else if(juego==2){
-    
-    snake();
-      
-    }
-  
+
+    switch (opcion)
+     {
+     case 1:
+     {
+      starShip();
+         break;
+     }
+     case 2:
+      snake();
+         break;
+      default:
+        cout<<"Ingrese una opcion entre 1 y 2"<<endl;
+         break;
+     }
+   
     return 0;
 }
